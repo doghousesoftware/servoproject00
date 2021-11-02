4ddd63c0dda8e162cbc98b884ad88b4bd9bc7002
bryan weinstein <bryanweinstein@outlook.com>
2021-11-02 14:45:07 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,56 @@
+input.onPinPressed(TouchPin.P0, function () {
+    countSpeed(0)
+})
 function stdServo (angle: number) {
     servos.P0.setAngle(angle)
 }
+function countAngle (incdec: number) {
+    if (incdec == 1) {
+        if (angleCounter < 180 && angleCounter >= 0) {
+            angleCounter += 10
+            basic.showNumber(angleCounter)
+            stdServo(angleCounter)
+        } else {
+            basic.showNumber(angleCounter)
+        }
+    } else {
+        if (angleCounter < 180 && angleCounter >= 0) {
+            angleCounter += 0 - 10
+            basic.showNumber(angleCounter)
+            stdServo(angleCounter)
+        } else {
+            basic.showNumber(angleCounter)
+        }
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    speedCounter += 10
-    contServo(speedCounter)
+    countSpeed(1)
 })
 input.onButtonPressed(Button.B, function () {
-    angleCounter += 10
-    stdServo(angleCounter)
+    countAngle(1)
 })
+input.onPinPressed(TouchPin.P1, function () {
+    countAngle(0)
+})
+function countSpeed (incdec: number) {
+    if (incdec == 1) {
+        if (speedCounter < 100 && speedCounter > -100) {
+            speedCounter += 10
+            basic.showNumber(speedCounter)
+            contServo(speedCounter)
+        } else {
+            basic.showNumber(speedCounter)
+        }
+    } else {
+        if (speedCounter < 100 && speedCounter > -100) {
+            speedCounter += 0 - 10
+            basic.showNumber(speedCounter)
+            contServo(speedCounter)
+        } else {
+            basic.showNumber(speedCounter)
+        }
+    }
+}
 function contServo (speed: number) {
     servos.P0.run(speed)
 }
